738d617846e5b296dd9b65c228f4768f5c7286d7
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-10-11 01:04:42 -0700

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A script for a type-annotated function 'add' that takes two floats
+as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
