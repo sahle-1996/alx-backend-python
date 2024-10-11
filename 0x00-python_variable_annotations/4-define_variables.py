8927847f8bca9732701134975b446ccf8a535ea1
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-10-11 03:04:57 -0700

    definevariables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This script defines several variables with specified values and type annotations.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
