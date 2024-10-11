ce1e6372c380949fdb15fff557d4b34ff00e6b72
sahle-1996 <sahlesiumgoitom@gmail.com>
2024-10-11 03:00:48 -0700

    tostr

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A script defining a type-annotated function 'to_str' that takes
+a float as input and returns its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string form.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
